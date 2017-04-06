279afc058abd9b062ba24c7eca3f59e18b419b08
kwebbink <magpiedin@gmail.com>
2017-04-06 17:35:47 -0500

    Updated raw data import

--- dash010CatPrep.R
+++ dash010CatPrep.R
@@ -1,65 +1,51 @@
 ## EMu Data Prep Script -- to prep exported table-field data for re-import
-#...in cases where need to overwrite whole table
-#   (in order to not duplicate rows/get stuff out of sync if nested/multivalue-table)
+#
+# 1) In EMu, retrieve Catalogue records for dashboard, 
+#       06-Apr-2017 dataset includes all ecatalogue records where:
+#           + (CatDepartment = "Anthropology" & CatLegalStatus = "Permanent Collection") |
+#           + (CatDepartment = "Zoology" | "Botany" | "Geology")  &  AdmPublishWebNoPassword = "Yes"
+#
+# 2) Report them out with "IPT dashboard" report
+#       - see collections-dashboard "Help" page for details on which fields are included in report
+#       - Best to report out 200k records at a time.
+#       - Rename "Group1.csv" as "Group1_[sequence-number].csv", and keep in one folder
+#           NOTE - Sequence numbering method does not matter as long as names are unique.
+#                - "Group" is the only required term in the CSV filenames.
+#
+# 3) Run this script  
+#       - NOTE: need to set working directory to folder containing "Group" csv's
+#         (see line 20)
 
 # install.packages("tidyr")  # uncomment if not already installed
 library("tidyr")
 
-# point to your csv's directory
-setwd("C:\\Users\\kwebbink\\Desktop\\IPTdashbdTest")
+# point to the directory containg the set of "Group" csv's from EMu
+#setwd("C:\\Users\\kwebbink\\Desktop\\IPTdashbdTest")
+setwd(paste0(getwd(),"/data01raw/emu/"))
 
-# point to your csv file(s)
-IPTlist = list.files(pattern="Group.*.csv$")
-CatDash <- do.call(rbind, lapply(IPTlist, read.csv))
-# CHECK IF NEED A WAY TO SET STRINGSASFACTORS=FALSE
+DashList = list.files(pattern="Group.*.csv$")
+CatDash <- do.call(rbind, lapply(DashList, read.csv, stringsAsFactors = F))
 
-NROW(levels(as.factor(CatDash$DarGlobalUniqueIdentifier)))
+setwd("..")  # up to /collprep/data01ra/
+setwd("..")  # up to /collprep/
 
-CatDash2 <- CatDash[order(CatDash$DarGlobalUniqueIdentifier),]
-CatDash2$GUIDseq <- sequence(rle(as.character(CatDash2$DarGlobalUniqueIdentifier))$lengths)
 
-CatDash3 <- CatDash2[which(nchar(as.character(CatDash2$DarGlobalUniqueIdentifier)) > 3 & CatDash2$GUIDseq == 1),]
-CatCheck <- CatDash2[which(CatDash2$GUIDseq > 2),]
+CatDash2 <- CatDash[order(CatDash$irn),-c(1,2)]
+CatDash2 <- unique(CatDash2)
 
-#CatDash3 <- unique(CatDash3)
 
-#CatxBasOfRec <- read.csv(file="ecatBasisOfRec.csv", stringsAsFactors = F)
-#CatxCatProj <- read.csv(file="ecatCatProj.csv", stringsAsFactors = F)
-#CatxDarYr <- read.csv(file="ecatDarYrColl.csv", stringsAsFactors = F)
-#CatxDarMo <- read.csv(file="ecatDarMoColl.csv", stringsAsFactors = F)
-#CatxDesK <- read.csv(file="ecatDesKDesc.csv", stringsAsFactors = F)
+# Remove duplicate irn's
+CatIRNcount <- NROW(levels(as.factor(CatDash2$irn)))
 
-#CatxBasOfRec <- CatxBasOfRec[,-1]
-#CatxCatProj <- CatxCatProj[,-1]
-#CatxDarYr <- CatxDarYr[,-1]
-#CatxDarMo <- CatxDarMo[,-1]
-#CatxDesK <- CatxDesK[,-1]
+CatDash2$IRNseq <- sequence(rle(as.character(CatDash2$irn))$lengths)
 
-#CatxBasOfRec <- unique(CatxBasOfRec)
-#CatxCatProj <- unique(CatxCatProj)
-#CatxDarYr <- unique(CatxDarYr)
-#CatxDarMo <- unique(CatxDarMo)
-#CatxDesK <- unique(CatxDesK)
+#CatDash3 <- CatDash2[which(nchar(as.character(CatDash2$DarGlobalUniqueIdentifier)) > 3 & CatDash2$IRNseq == 1),]
+CatDash3 <- CatDash2[which(CatDash2$IRNseq == 1),]
+CatCheck <- CatDash2[which(CatDash2$IRNseq > 1),]
 
-##IPTlatlon <- read.csv(file="dashbdLatLong.csv")
-##IPTlatlon <- IPTlatlon[,2:4]
-
-#CatDash3 <-  subset(CatDash3, select=-c(GUIDseq))
-
-#CatDash2 <- merge(IPTlatlon, CatDash2, by="irn", all.y=T)
-#rm(IPTlatlon)
-
-#CatDash3 <- merge(CatDash3, CatxBasOfRec, by = "irn", all.x=T)
-#CatDash3 <- merge(CatDash3, CatxCatProj, by = "irn", all.x=T)
-#CatDash3 <- merge(CatDash3, CatxDarYr, by = "irn", all.x=T)
-#CatDash3 <- merge(CatDash3, CatxDarMo, by = "irn", all.x=T)
-#CatDash3 <- merge(CatDash3, CatxDesK, by = "irn", all.x=T)
-#rm(CatxBasOfRec, CatxCatProj, CatxDarYr, CatxDarMo, CatxDesK)
-
-CatDash3 <- unique(CatDash3)
-
+CatDash3 <- dplyr::select(CatDash3, -IRNseq)
 
 
 # write the lumped/full/single CSV back out
-write.csv(CatDash3[,3:(NCOL(CatDash3)-1)], file="CatDash3bu.csv", row.names = F)
-write.csv(CatCheck[,3:NCOL(CatCheck)], file="CatDupGUIDs.csv", row.names = F)
+write.csv(CatDash3, file="CatDash3bu.csv", row.names = F)
+#write.csv(CatCheck, file="CatCheckIRN.csv", row.names = F)
