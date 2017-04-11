c830cbb29d79d09a502d7fec8ab6b9341333754c
kwebbink <magpiedin@gmail.com>
2017-04-11 15:48:05 -0500

    Added working directory set-up options

--- Master.R
+++ Master.R
@@ -1,4 +1,27 @@
-setwd(paste0(getwd(),"/collprep/"))
+# Run this script to prep a Collections Dashboard dataset.
+
+#origdir <- getwd()
+origdir <- "D:/colldash2/collprep/"
+
+# Instructions
+print("Step 1 - Save scripts repo to computer.")
+print("Step 2 - Save raw data to sub-folder called 'data01raw'.")
+print(paste("Step 3 - Set working directory.  Currently it is set to: ", getwd()))
+switchYN <- readline(prompt="Do you need to change the working directory to the folder containing the scripts? (Y/N) ")
+
+if (switchYN=="Y") {
+  origdir <- readline(prompt="Enter working directory (e.g., C:/path/to/folder ) ")
+  setwd(origdir)
+} else if (switchYN=="N") {
+  setwd(origdir)
+}
+
+if (dir.exists(paste0(origdir,"/output"))==T) {
+  print("Step 4 - 'output' directory already set up.")
+} else if (dir.exists(paste0(origdir,"/output"))==F) {
+  dir.create("./output", showWarnings = T)
+  print("Step 4 - 'output' directory created.")
+}
 
 
 sourceDir <- function(path, trace = TRUE, ...) {
@@ -23,5 +46,5 @@ source("dash020FullBind.R")
 source("dash021Where.R")
 source("dash022What.R")
 source("dash023When.R")
-#  ADD dash024Who.R
+source("dash024Who.R")
 source("dash030FullExport.R")
